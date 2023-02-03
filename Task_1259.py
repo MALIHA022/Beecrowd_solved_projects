10c5c941a5f356de176286ba1e05fc4d95fa15a6
MALIHA022 <114968869+MALIHA022@users.noreply.github.com>
2023-02-04 02:02:26 +0600

    Update Task_1259.py

--- Task_1259.py
+++ Task_1259.py
@@ -1,22 +1,22 @@
 #1259
-num_of_inp = int(input("Enter number of inputs: "))
+num_of_inp = int(input())
 even = []
 odd = []
 output = []
-for i in range(num_of_inp):   #number of iterations
-    numbers= int(input("Enter numbers: "))  #vertically taken input
+for i in range(num_of_inp):   
+    numbers= int(input())  
     if numbers%2 == 0:
          even.append(numbers)
     else:
         odd.append(numbers)
-even.sort()   #sorted list of even numbers
-odd.sort(reverse=True)  #reverse sorted list of odd numbers
+even.sort()  
+odd.sort(reverse=True)  
 
 for nums in even:
     output.append(nums)
 for nums in odd:
     output.append(nums)
-# print(output)  #new list of numbers
+
 
 for i in output:
-    print(i)       #vertical output
+    print(i)      
